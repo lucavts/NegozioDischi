bf19c37d15cb5e6cb7997f409518c8c1f85b0ddd
lucavts <lucanots@hotmail.it>
2017-02-17 19:46:19 +0100

    Aggiunto commento alla linea 78, aggiunti cicli per scegliere se visualizzare o inserire i dischi.

--- Dischi/src/NegozioMain.java
+++ Dischi/src/NegozioMain.java
@@ -1,8 +1,12 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
 public class NegozioMain {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) 
 	{
+		int c;
+		Scanner s;
 		ArrayList<ShowName> disk = new ArrayList <ShowName>();
 		ArrayList<ShowYear> year = new ArrayList <ShowYear>();
 		disk.add(new ShowName("Album 1"));
@@ -29,12 +33,47 @@ public class NegozioMain {
 		year.add(new ShowYear (2003));
 		year.add(new ShowYear (2016));
 		year.add(new ShowYear (2017));
+		System.out.println("Premi 1 per visualizzare i dischi, premi 2 per inserirli");
+		System.out.print("Input: ");
+		Scanner input= new Scanner(System.in);
+		c=input.nextInt();
+		System.out.println(c);
+		//switch (c)
+		//{
 		//for (int i=0, i<disco)
-		for (int i=0;i<disk.size();i++)// OUTPUT
+		//case '1':
+		if (c==1)
 		{
-			disk.get(i).PrintName();
-			year.get(i).PrintYear();
+			for (int i=0;i<disk.size();i++)// OUTPUT
+			{
+				disk.get(i).PrintName();
+				year.get(i).PrintYear();
+			}
 		}
-	}
+			//break;
+		//case '2':
+		if (c==2)
+		{
+			System.out.print("Quanti dischi vuoi aggiungere? ");
+			Scanner input2 = new Scanner (System.in);
+			int num=input2.nextInt();
+			int cont1=1;
+			do
+			{
+				System.out.print("Indica il nome dell'album: ");
+				Scanner inputalbum= new Scanner (System.in);		
+				String namealbum=inputalbum.nextLine();
+				System.out.print("Indica la data di uscita dell'album: ");
+				Scanner inputdata= new Scanner (System.in);
+				int yearalbum=inputdata.nextInt();
+				disk.add(new ShowName(namealbum));
+				year.add(new ShowYear (yearalbum));
+				cont1++;
+			}
+			while (cont1==num);
+			//break;
+		//}
+		}
+		}
 
 }
